47ff56cf6fc43fda1cfd6415bca151eec21ef72b
Bryant López <bmiguel0170@gmail.com>
2024-05-06 07:36:33 -0700

    seleccionarRegistro

--- ITDS071_BDFina;l/Program.cs
+++ ITDS071_BDFina;l/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -19,12 +20,14 @@ namespace ITDS071_BDFina_l
                 Titulos = titulos;
                 Registros = registros;
                 Ruta = ruta;
+                FilaSeleccionada = 0;
             }
 
             public string Nombre {  get; set; }
             public string[] Titulos { get; set; }
             public List<String[]> Registros { get; set; }
             public string Ruta { get; set; }
+            public int FilaSeleccionada { get; set; }
         };
             
 
@@ -54,8 +57,20 @@ namespace ITDS071_BDFina_l
                 );
             leerArchivo(Recetas);
 
-            dibujarTabla(Recetas, 1, 1);
-            Console.ReadKey(true);
+            while (true)
+            {
+                Console.Clear();
+                dibujarTabla(Ingredientes, 1, 1);
+                string tecla = Console.ReadLine();
+                if (tecla == "x")
+                {
+                    eliminarRegistro(Ingredientes);
+                    Ingredientes.FilaSeleccionada = seleccionarRegistro(Ingredientes, 0);
+                    continue;
+                }
+                int contador = int.Parse(tecla);
+                Ingredientes.FilaSeleccionada = seleccionarRegistro(Ingredientes, contador);
+            }
         }
 
         static void leerArchivo(Tabla tabla)
@@ -121,41 +136,79 @@ namespace ITDS071_BDFina_l
             filaActual++;
 
             // Dibujar registros
-            if (tabla.Registros.Count > 0)
+            int indiceRegistro = 0;
+            foreach (string[] registro in tabla.Registros)
             {
-                string bordeRegistro = "╠";
-                string lineaRegistro = "║";
-                foreach (string[] registro in tabla.Registros)
+                Console.SetCursorPosition(x, filaActual);
+                Console.Write("╠"); 
+                for (int i = 0; i < tabla.Titulos.Length; i++)
                 {
-                    ultimo = registro.Last();
-                    foreach (string dato in registro)
+                    if (i == tabla.Titulos.Length - 1)
                     {
-                        if (dato == ultimo)
-                        {
-                            bordeRegistro += "═══════════════╣";
-                            if (dato.Length >= 15) lineaRegistro += dato.Remove(15);
-                            else lineaRegistro += dato.PadRight(15, ' ');
-                        }
-                        else
-                        {
-                            bordeRegistro += "═══════════════╬";
-                            if (dato.Length >= 15) lineaRegistro += dato.Remove(15);
-                            else lineaRegistro += dato.PadRight(15, ' ');
-                        }
-                        lineaRegistro += "║";
+                        Console.Write("═══════════════╣");
+                    }
+                    else
+                    {
+                        Console.Write("═══════════════╬");
                     }
                 }
-                Console.SetCursorPosition(x, filaActual);
-                Console.Write(bordeRegistro);
                 filaActual++;
                 Console.SetCursorPosition(x, filaActual);
-                Console.Write(lineaRegistro);
+                if (indiceRegistro == tabla.FilaSeleccionada) Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Write("║");
+                foreach (string dato in registro)
+                {
+                    string datoFinal = dato;
+                    if (datoFinal.Length >= 15) datoFinal = datoFinal.Remove(15); 
+                    Console.Write($"{datoFinal.PadRight(15, ' ')}║");
+                }
                 filaActual++;
+                indiceRegistro++;
+                Console.ResetColor();
             }
 
             // Dibujar borde inferior
             Console.SetCursorPosition(x, filaActual);
             Console.Write(bordeInferior);
+        }
+
+        static string[] agregarRegistro(Tabla tabla, int x, int y, char v)
+        {
+            string[] datos = new string[tabla.Titulos.Length];
+            for (int i = 0; i < tabla.Titulos.Length; i++)
+            {
+                if (tabla.Titulos[i] == "Costo")
+                {
+                    datos[i] = "0";
+                }
+                else
+                {
+                    Console.SetCursorPosition(x, y + i);
+                    Console.Write($"{tabla.Titulos[i]}: ");
+                    datos[i] = Console.ReadLine();
+                }
+            }
+            string linea = "\n" + string.Join(",", datos);
+            File.AppendAllText(tabla.Ruta, linea);
+            return datos;
+        }
+
+        static int seleccionarRegistro(Tabla tabla, int contador)
+        {
+            int cantidadDeRegistros = tabla.Registros.Count();
+            if (cantidadDeRegistros == 0) return 0;
+            int filaSeleccionada = tabla.FilaSeleccionada;
+            filaSeleccionada += contador;
+
+            if (filaSeleccionada < 0) filaSeleccionada = cantidadDeRegistros - 1;
+            filaSeleccionada = filaSeleccionada % cantidadDeRegistros;
+            
+            return filaSeleccionada;
+        }
+
+        static void eliminarRegistro(Tabla tabla)
+        {
+            tabla.Registros.RemoveAt(tabla.FilaSeleccionada);
         }
     }
 }
